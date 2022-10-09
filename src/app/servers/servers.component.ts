22f841015e4831d79bfe35ef306d9dc5e82ade50
Muhammad Arshaq <arshaqshakeel3@gmail.com>
2022-10-09 14:43:29 +0500

    learn external and inline styling in angular

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  // templateUrl: './servers.component.html',
+  template: '<app-server> </app-server> <h2>Hello</h2>', // for multi line code use es6 template literals
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
