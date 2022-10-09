b4ba6d6564d0d0f2671c15ace98ae257ebf929a7
Muhammad Arshaq <arshaqshakeel3@gmail.com>
2022-10-10 00:45:33 +0500

    Learn event binding in angular

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,15 +2,25 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  // templateUrl: './servers.component.html',
-  template: '<app-server> </app-server> <h2>Hello</h2>', // for multi line code use es6 template literals
+  templateUrl: './servers.component.html',
+  // template: '<app-server> </app-server> <h2>Hello</h2>', // for multi line code use es6 template literals
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = 'No Server Was Created!'
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+  }
 
   ngOnInit(): void {
+  }
+
+
+  onServerCreated() {
+    this.serverCreationStatus = this.serverCreationStatus ==='Server Was Created!' ? 'No Server Was Created!': 'Server Was Created!'
   }
 
 }
