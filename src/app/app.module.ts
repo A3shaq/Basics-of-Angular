f0657342024f17d3168254bdfa1f9d87bf7039f0
Muhammad Arshaq <arshaqshakeel3@gmail.com>
2022-10-11 00:14:27 +0500

    Create all require components for the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from "@angular/forms"
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+import { HeaderComponent } from './header/header.component';
+import { RecepiesComponent } from './recepies/recepies.component';
+import { RecipeListComponent } from './recepies/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recepies/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recepies/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    HeaderComponent,
+    RecepiesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
