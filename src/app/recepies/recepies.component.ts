686247eb7664c26702a8ec80ffe8eb174aaa3717
Muhammad Arshaq <arshaqshakeel3@gmail.com>
2022-10-11 15:35:28 +0500

    render recipe component

--- src/app/recepies/recepies.component.ts
+++ src/app/recepies/recepies.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recepies',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recepies.component.css']
 })
 export class RecepiesComponent implements OnInit {
-
+  imageUrl : string = 'https://img.jamieoliver.com/jamieoliver/recipe-database/136295678.jpg?tr=w-800,h-1066'
+  recipes: Recipe[] = [new Recipe("The Test Recipe", "Best things", this.imageUrl)]
   constructor() { }
 
   ngOnInit(): void {
