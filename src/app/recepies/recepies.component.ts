a362958b7f0d9adeaf3e584c008fe7ccff2e4b44
Muhammad Arshaq <arshaqshakeel3@gmail.com>
2022-10-12 14:30:58 +0500

    render recipes list using *ng-for

--- src/app/recepies/recepies.component.ts
+++ src/app/recepies/recepies.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { Recipe } from './recipe.model';
+import { Component, OnInit } from '@angular/core';;
 
 @Component({
   selector: 'app-recepies',
@@ -7,8 +6,7 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recepies.component.css']
 })
 export class RecepiesComponent implements OnInit {
-  imageUrl : string = 'https://img.jamieoliver.com/jamieoliver/recipe-database/136295678.jpg?tr=w-800,h-1066'
-  recipes: Recipe[] = [new Recipe("The Test Recipe", "Best things", this.imageUrl)]
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/recepies/recipe-list/recipe-list.component.ts
+++ src/app/recepies/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  // imageUrl : string = 'https://img.jamieoliver.com/jamieoliver/recipe-database/136295678.jpg?tr=w-800,h-1066'
+  recipes: Recipe[] = [new Recipe("The Test Recipe", "Best things", 'https://img.jamieoliver.com/jamieoliver/recipe-database/136295678.jpg?tr=w-800,h-1066',),
+  new Recipe("The Test Recipe", "Best things", 'https://img.jamieoliver.com/jamieoliver/recipe-database/136295678.jpg?tr=w-800,h-1066',)]
   constructor() { }
 
   ngOnInit(): void {
