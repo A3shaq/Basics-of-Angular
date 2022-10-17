b42419c63189909d34238e2639536ca5fbdc6792
Muhammad Arshaq <arshaqshakeel3@gmail.com>
2022-10-17 13:18:22 +0500

    Angular life cyle hooks

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
@@ -6,11 +6,28 @@ import { Ingredient } from '../shared/ingredient.model';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [new Ingredient('Banana', 10),new Ingredient('Apple', 5)]
-  constructor() { }
+export class ShoppingListComponent implements OnInit, OnDestroy, AfterContentInit, AfterViewInit {
+  ingredients: Ingredient[] = [new Ingredient('Banana', 10), new Ingredient('Apple', 22)]
+  constructor() {
+    console.log('constructor')
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit')
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy')
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit')
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit')
   }
 
 }
