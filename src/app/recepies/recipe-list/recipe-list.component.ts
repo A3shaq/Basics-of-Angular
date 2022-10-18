0e4e6fdeaea57915c4a9fdbd3398cf9e37f898f5
Muhammad Arshaq <arshaqshakeel3@gmail.com>
2022-10-18 12:34:44 +0500

    redirect using programatically

--- src/app/recepies/recipe-list/recipe-list.component.ts
+++ src/app/recepies/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -10,9 +11,18 @@ export class RecipeListComponent implements OnInit {
   // imageUrl : string = 'https://img.jamieoliver.com/jamieoliver/recipe-database/136295678.jpg?tr=w-800,h-1066'
   recipes: Recipe[] = [new Recipe("The Test Recipe", "Best things", 'https://img.jamieoliver.com/jamieoliver/recipe-database/136295678.jpg?tr=w-800,h-1066',),
   new Recipe("The Test Recipe", "Best things", 'https://img.jamieoliver.com/jamieoliver/recipe-database/136295678.jpg?tr=w-800,h-1066',)]
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+  }
+  redirectToDetails(): void {
+   //for redirect logic
+   this.router.navigateByUrl('/recipes/details')
+  //  this.router.
+  }
+
+  goBack(){
+    // this._location.go();
   }
 
 }
